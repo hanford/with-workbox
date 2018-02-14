9464e28afc97551bc22b90153d0a9f1eead81e30
hanford <jackhanford@gmail.com>
2018-02-13 22:51:55 -0800

    cleanup config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@ const withOffline = require('next-offline')
 
 module.exports = withOffline({
   webpack (config) {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
-
     return config
   }
 })
